fce4f85709d3f496c39b68c14cd3d31e6e460cbb
Abaadir <abaadir.yasin@student.moringaschool.com>
2023-11-23 10:24:01 +0300

    completed

--- movie-app/src/components/Search.js
+++ movie-app/src/components/Search.js
@@ -6,3 +6,28 @@ const Search = ({ handleSearch }) => {
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
+  const handleClick = () => {
+    handleSearch(searchTerm);
+  };
+
+  return (
+    <div className="row d-flex align-items-center mt-4 mb-4">
+      <div className="col">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search for a movie..."
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="col-auto">
+        <button className="btn btn-primary" onClick={handleClick}>
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
